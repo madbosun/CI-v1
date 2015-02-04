72e14946f343d155f55dd27d0f12e8d12871ccd0
Jason Byrd <jasonnbyrd@gmail.com>
2015-02-04 10:38:32 -0500

    dual level threshold

--- src/include/frag_def.h
+++ src/include/frag_def.h
@@ -7,12 +7,13 @@
        logical frag_calc
        integer nfrags, natoms_frag, watom_frag
        double precision r_thresh 
+       double precision elst_thresh 
        common /FRAGDEF/frag_calc, nfrags, 
      *                natoms_frag(maxfrag),
      *                watom_frag(maxfrag,maxapf) 
      *
 
-       common /FRAG_THRESH/r_thresh 
+       common /FRAG_THRESH/r_thresh,elst_thresh
  
        integer fragAO, n_occ, n_aocc, n_bocc, n_virt, n_avirt, n_bvirt
        common /FORBS/fragAO(maxfrag), 
